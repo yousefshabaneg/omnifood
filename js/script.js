4efb4081c673def7b7b459e4a74e488c537b3b4e
Yousef Shaban <yousefshaban.eg@gmail.com>
2022-07-31 02:26:01 +0200

    Implemented the smooth scrolling.

--- js/script.js
+++ js/script.js
@@ -14,7 +14,41 @@ const headerEl = document.querySelector('.header');
 btnNavEl.addEventListener('click', 
 () => headerEl.classList.toggle("nav-open")
 );
-("nav-open");
+
+
+///////////////////////////////////////////////////////////
+// Smooth Scrolling Animation
+
+const allLinks = document.querySelectorAll('a');
+
+allLinks.forEach(function(link){
+  link.addEventListener('click', function (e) {
+    e.preventDefault(); //Reset the lik element to do nothing (no scrolling)
+
+    const href = link.getAttribute('href');
+    
+    //Scroll Back to top
+    if(href == "#"){
+      window.scrollTo({
+        top:0,
+        behavior: 'smooth',
+      });
+    }
+
+    //Scroll to other links
+    if(href !== "#" && href.startsWith('#')){
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({behavior:"smooth"});
+    }
+
+    //Close mobile navigation
+    if(link.classList.contains("main-nav-link")){
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+})
+
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
